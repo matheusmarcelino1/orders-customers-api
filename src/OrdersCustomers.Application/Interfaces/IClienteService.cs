0d90d8d50d3ad9c41f2c1517dcc375ba2802ddfd
matheusmarcelino1 <matheusdantas9@icloud.com>
2026-02-11 11:11:11 -0300

    feat - criação das interfaces

---
+++ src/OrdersCustomers.Application/Interfaces/IClienteService.cs
@@ -0,0 +1,16 @@
+﻿using OrdersCustomers.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersCustomers.Application.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<ClienteDto> CreateAsync(CreateClienteDto dto);
+        Task<List<ClienteDto>> GetAllAsync();
+        Task<ClienteDto?> GetByIdAsync(Guid id);
+    }
+}
---
+++ src/OrdersCustomers.Application/Interfaces/IPedidoService.cs
@@ -0,0 +1,16 @@
+﻿using OrdersCustomers.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersCustomers.Application.Interfaces
+{
+    public interface IPedidoService
+    {
+        Task<PedidoDto> CreateAsync(CreatePedidoDto dto);
+        Task<List<PedidoDto>> GetAllAsync();
+        Task<PedidoDto?> GetByIdAsync(Guid id);
+    }
+}
