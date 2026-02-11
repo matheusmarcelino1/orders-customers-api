7c98b0828496662e452a90c506ae3afbf5045e49
matheusmarcelino1 <matheusdantas9@icloud.com>
2026-02-11 11:59:05 -0300

    feat - chamada da controller

---
+++ src/OrdersCustomers.Api/Controllers/ClienteController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdersCustomers.Application.DTOs;
+using OrdersCustomers.Application.Interfaces;
+
+namespace OrdersCustomers.Api.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _service;
+
+        public ClienteController(IClienteService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClienteDto>> Create(CreateClienteDto dto)
+        {
+            var cliente = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = cliente.Id }, cliente);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ClienteDto>>> GetAll()
+        {
+            var clientes = await _service.GetAllAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClienteDto>> GetById(Guid id)
+        {
+            var cliente = await _service.GetByIdAsync(id);
+
+            if (cliente == null)
+                return NotFound();
+
+            return Ok(cliente);
+        }
+    }
+}
---
+++ src/OrdersCustomers.Api/Controllers/PedidoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdersCustomers.Application.DTOs;
+using OrdersCustomers.Application.Interfaces;
+
+namespace OrdersCustomers.Api.Controllers
+{
+    [ApiController]
+    [Route("pedidos")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoService _service;
+
+        public PedidoController(IPedidoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PedidoDto>> Create(CreatePedidoDto dto)
+        {
+            var pedido = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = pedido.Id }, pedido);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PedidoDto>>> GetAll()
+        {
+            var pedidos = await _service.GetAllAsync();
+            return Ok(pedidos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PedidoDto>> GetById(Guid id)
+        {
+            var pedido = await _service.GetByIdAsync(id);
+
+            if (pedido == null)
+                return NotFound();
+
+            return Ok(pedido);
+        }
+    }
+}
