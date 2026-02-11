cd4d879e83c3352cff4b65b26865e79cc6e74ec8
matheusmarcelino1 <matheusdantas9@icloud.com>
2026-02-11 11:05:47 -0300

    feat - conexão com o banco de dados

--- src/OrdersCustomers.Api/Program.cs
+++ src/OrdersCustomers.Api/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using OrdersCustomers.Infrastructure.Data;
+using OrdersCustomers.Domain.Interfaces;
+using OrdersCustomers.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=orderscustomers.db"));
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 
 var app = builder.Build();
 
@@ -22,4 +33,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
